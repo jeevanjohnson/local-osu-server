75042cbba830b3f3189cf38c1d55f5196f58fc05
James Tepper <jamesatepper@gmail.com>
2024-06-23 23:15:19 -0400

    began login request & general cleanup

--- server/main.py
+++ server/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 import uvicorn
-import settings
 from fastapi.staticfiles import StaticFiles
 from contextlib import asynccontextmanager
 from fastapi.logger import logger
+# import settings
 # import pipreqs
 
 # TODO: Use Relic for data
@@ -16,32 +16,33 @@ from fastapi.logger import logger
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    if settings.DEVELOPER:
-        logger.info("Running on Developer Mode")
-    
+    # if settings.DEVELOPER:
+    #     logger.info("Running on Developer Mode")
+
     # add all necessary routerefs
     import frontend
     import osu_client
     import api
-    
+
     app.include_router(frontend.web_client_router)
     app.include_router(osu_client.bancho_handling_router)
     app.include_router(api.api_authentication_router)
 
     # static is the default folder that fastapi uses for grabbing
     # javascript and css files
-    web_path_for_css_and_javascript_files = '/static'
-    app.mount(
-        web_path_for_css_and_javascript_files, 
-        StaticFiles(directory='./server/frontend/static'), 
-        name='static'
-    )
-    
+
+    # web_path_for_css_and_javascript_files = '/static'
+    # app.mount(
+    #     web_path_for_css_and_javascript_files,
+    #     StaticFiles(directory='./server/frontend/static'),
+    #     name='static'
+    # )
+
     # TODO: Add an easier method of connecting without mitmproxy
     # preferably the "cloudflare method"
 
-    yield 
-            
+    yield
+
 
 app = FastAPI(lifespan=lifespan)
 
@@ -49,5 +50,5 @@ app = FastAPI(lifespan=lifespan)
 def root():
     return {"message": "FastAPI server is running!"}
 
-if __name__ == "__main__":   
-    uvicorn.run("main:app", port=8000)#, #reload=True)
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, reload=True)
